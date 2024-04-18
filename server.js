c3f73aaf4d6cf4886f3cf2c50807e28342d3b5af
Marxu <fuhaojun@52tt.com>
2024-04-18 16:25:49 +0800

    feat:  客户端引入 socket.io cdn 并 配置 {     cors: {       origin: '*',     }   } 解决 跨域问题

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const http = require('http');
+const server = http.createServer(); // 创建HTTP服务器实例
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+  
 
 const PORT = 3000;
 
 server.listen(PORT);
-console.log(`Server listening on ${PORT}`);
+console.log(`Listening on port ${PORT}...`);
 
 io.on('connection', (socket) => {
-    console.log('One user connected');
+  console.log('a user connected');
 });
